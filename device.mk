RaghuVarma331/android_device_motorola_hanoip-TWRP

hanoip: Merge prebuilt kernel-headers https://android.googlesource.com/device/google/sunfish-kernel/+/refs/tags/android-11.0.0_r48/sm7150/original-kernel-headers
Signed-off-by: RaghuVarma331 <raghuvarma331@gmail.com>

device.mk
@@ -22,7 +22,7 @@ PRODUCT_SOONG_NAMESPACES += \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # Prebuilt headers
-PRODUCT_VENDOR_KERNEL_HEADERS := kernel/motorola/kernel-headers
+PRODUCT_VENDOR_KERNEL_HEADERS := $(LOCAL_PATH)/kernel-headers
 
 # A/B
 AB_OTA_UPDATER := true

anoosragh69/twrp_device_motorola_hanoip

device.mk
@@ -87,7 +87,7 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/commonsys-intf/display
 
 # Prebuilt headers
-PRODUCT_VENDOR_KERNEL_HEADERS := kernel/motorola/kernel-headers
+PRODUCT_VENDOR_KERNEL_HEADERS := $(DEVICE_PATH)/kernel-headers
 
 # Blacklist
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST += \