ef2f9b89f3b8a8009b132c19343786ec748fa8fb
RaghuVarma331 <raghuvarma331@gmail.com>
2024-05-04 19:53:55 +0530

    hanoip:use touchscreen universal fix
    commit taken from :
    https://github.com/RaghuVarma331/android_device_motorola_hanoip-TWRP/commit/6770e297e61d55204bafac08e05b44b523d60d34
    https://github.com/RaghuVarma331/android_device_motorola_hanoip-TWRP/commit/8a63d04dface2accf03e5c35f75c679d15f4995b

--- recovery/root/system/bin/hanoip_touch.sh
+++ recovery/root/system/bin/hanoip_touch.sh
@@ -1,36 +1,8 @@
 #!/system/bin/sh
 
-module_path=/vendor/lib/modules
-
+module_path=/sbin/modules
+firmware_path=/sbin/firmware
 touch_class_path=/sys/class/touchscreen
-touch_path=
-firmware_path=/vendor/firmware
-firmware_file=
-device=$(getprop ro.boot.device)
-
-wait_for_poweron()
-{
-	local wait_nomore
-	local readiness
-	local count
-	wait_nomore=60
-	count=0
-	while true; do
-		readiness=$(cat $touch_path/poweron)
-		if [ "$readiness" == "1" ]; then
-			break;
-		fi
-		count=$((count+1))
-		[ $count -eq $wait_nomore ] && break
-		sleep 1
-	done
-	if [ $count -eq $wait_nomore ]; then
-		return 1
-	fi
-	return 0
-}
-
-# Load all needed modules
 insmod $module_path/exfat.ko
 insmod $module_path/ilitek_v3_mmi.ko
 insmod $module_path/mmi_info.ko
@@ -43,18 +15,13 @@ insmod $module_path/mmi_sys_temp.ko
 insmod $module_path/mpq-adapter.ko
 insmod $module_path/sensors_class.ko
 insmod $module_path/utags.ko
-
 cd $firmware_path
 touch_product_string=$(ls $touch_class_path)
-insmod $module_path/aw8646.ko
+echo "ilitek"
 firmware_file="FW_ILITEK_TDDI_TM.bin"
-
-touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk -Fliber '{print $1}')
+touch_path=/sys$(cat $touch_class_path/$touch_product_string/path | awk '{print $1}')
 wait_for_poweron
 echo $firmware_file > $touch_path/doreflash
 echo 1 > $touch_path/forcereflash
 sleep 5
 echo 1 > $touch_path/reset
-
-return 0
-
